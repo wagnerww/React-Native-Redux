28d32f8ee36b7d96cbcead500c5d85a5f1a56618
wagner <wagnerricardonet@gmail.com>
2019-03-08 12:40:58 -0300

    atualizando o reactotron

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 
 import todos from "./reducers/todos";
 
-const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore;
-const store = createAppropriateStore(todos);
+const createAppropriateStore = __DEV__ ? console.tron.createEnhancer : () => {};
+
+const store = createStore(todos, compose(createAppropriateStore()));
 
 export default store;
